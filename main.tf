90a7d364dfe15f742510594da74121d939c24bd3
Nilesh <nk2135@gmail.com>
2023-09-15 11:15:48 +0530

    main tf modified

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "google_compute_instance" "default" {
   name         = var.instance_name
   machine_type = var.machine_type
   zone         = var.zone
-  boot_disk       = var.boot_disk
+  
 
   // ... (rest of your configuration)
 
