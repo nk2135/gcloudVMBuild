7daed3e3381928ef9ba853a70651c2cefb8525b9
Nilesh <nk2135@gmail.com>
2023-09-15 11:10:01 +0530

    main tf modified

--- main.tf
+++ main.tf
@@ -2,9 +2,18 @@ resource "google_compute_instance" "default" {
   name         = var.instance_name
   machine_type = var.machine_type
   zone         = var.zone
-  osdisk       = var.boot_disk
+  boot_disk       = var.boot_disk
 
   // ... (rest of your configuration)
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+}
 }
 
 
